932b52e9ba29fc92b69f1d992c6e3918e40552a7
Owen L - SFE <oclyke@gmail.com>
2019-03-05 10:48:49 -0700

    Rebuild examples pt. II - make all examples

---
+++ examples/SparkFun_Edge_Project_Template/gcc/bin/inc2.d
@@ -0,0 +1,3 @@
+bin/inc2.o: ../src/inc2/inc2.c ../src/inc2/inc2.h
+
+../src/inc2/inc2.h:
---
+++ examples/example1_edge_test/gcc/bin/lis2dh12_reg.d
@@ -0,0 +1,4 @@
+bin/lis2dh12_reg.o: ../src/tf_accelerometer/lis2dh12_reg.c \
+ ../src/tf_accelerometer/lis2dh12_reg.h
+
+../src/tf_accelerometer/lis2dh12_reg.h:
